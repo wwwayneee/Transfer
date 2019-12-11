71fd8a767c5a48f4c17173a03b6598f177728495
Yuxuan Chang <zhangyuxuan171@mails.ucas.ac.cn>
2019-12-11 15:58:36 +0800

    windows

--- subset.pl
+++ subset.pl
@@ -1,15 +1,16 @@
-domains
- ilist=integer*
- 
-predicates
- nondeterm subset(ilist, ilist)
- 
-clauses
+/*
  subset([], []).
  subset([X|Set], [X|SubSet]):-subset(Set, SubSet).
  subset([_|Set], SubSet):-subset(Set, SubSet).
  
-goal
+
  subset([1,2,3], Subset),
  write(Subset), nl,
  fail.
+*/
+
+subset([], []).
+subset([X|SubSet], [X|Set]):-subset(SubSet, Set).
+
+subset(A, [1, 2, 3]);
+write(A).
