8cd1cc67d6ab975efe66889802854c24bd268792
Yuxuan Chang <zhangyuxuan171@mails.ucas.ac.cn>
2019-12-11 16:39:07 +0800

    w

---
+++ elemnet.pl
@@ -0,0 +1,9 @@
+
+
+grade:-write("input a name"),
+		read(Name),
+		student(Name,Score),
+		write(Score).
+
+element:-member(A, [1, 2, 3, 4, 5]),
+		write(A).
--- grade.pl
+++ grade.pl
@@ -1,9 +1,10 @@
 student("zhangsan",23).
 student("lisi",243).
 student("wangwu",143).
-	
+
 grade:-write("input a name"),
 		read(Name),
 		student(Name,Score),
 		write(Score).
 grade:-write("no people").
+
