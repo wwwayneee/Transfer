9df7aa2cf309f6f1633d2196dc8e443269981551
Yuxuan Chang <zhangyuxuan171@mails.ucas.ac.cn>
2019-12-11 16:32:00 +0800

    w

--- grade.pl
+++ grade.pl
@@ -1,8 +1,9 @@
-student(1,"zhangsan",23).
-student(2,"lisi",243).
-student(3,"wangwu",143).	
+student("zhangsan",23).
+student("lisi",243).
+student("wangwu",143).
+	
 grade:-write("input a name"),
 		read(Name),
-		student(_,Name,Score),
+		student(Name,Score),
 		write(Score).
 grade:-write("no people").
