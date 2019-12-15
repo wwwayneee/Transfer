43840ba5921bdb81875569e3fb7850b183aa7d5c
Yuxuan Chang <zhangyuxuan171@mails.ucas.ac.cn>
2019-12-15 08:46:19 +0800

    w

--- lop1.pl
+++ lop1.pl
@@ -1,6 +1,6 @@
-student(1, "张三", 90.2).
-student(2, "李四", 95.5).
-student(3, "王五", 96.4).
+student(1, "ZhangSan", 90.2).
+student(2, "LiSi", 95.5).
+student(3, "WangWu", 96.4).
 print :- student(Number, Name, Score),
 		write(Number, Name, Score), nl,
 		Number = 3.
